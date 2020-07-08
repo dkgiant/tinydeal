07ed067f174f6afbaa47d33e54e6152ac581354a
dhgiant <dhgiang85@gmail.com>
2020-07-08 15:53:35 +0700

    final comit

--- tinydeal/settings.py
+++ tinydeal/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'tinydeal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tinydeal (+http://www.yourdomain.com)'
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -86,3 +86,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+FEED_EXPORT_ENCODING = 'UTF-8'
